e2b443875fbcba04df3e9a9eebeff9f1b98e8f9e
DevLintMar <LintMar@outlook.com>
2022-07-21 13:24:50 +0800

    Remove Unused

--- src/main/java/com/lintmar/springboot/Application.java
+++ src/main/java/com/lintmar/springboot/Application.java
@@ -2,14 +2,8 @@ package com.lintmar.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EnableAsync
-@EnableScheduling
 @SpringBootApplication
-@EnableTransactionManagement(proxyTargetClass = true)
 public class Application {
 
     public static void main(String[] args) {
