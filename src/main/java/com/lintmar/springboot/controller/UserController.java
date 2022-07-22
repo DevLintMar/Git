21dd151755b2f1722c860edb8c6ba71bf90429c9
DevLintMar <LintMar@outlook.com>
2022-07-22 17:12:24 +0800

    Version 2.0.1

--- src/main/java/com/lintmar/springboot/controller/UserController.java
+++ src/main/java/com/lintmar/springboot/controller/UserController.java
@@ -20,7 +20,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/main")
+    @RequestMapping({"", "/", "/main"})
     public String main() {
         return "user";
     }
